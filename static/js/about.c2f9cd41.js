a3dd472ab4be55d9138c4aef123f423e831f2f0a
樊家琦 <123469366+underRiverr@users.noreply.github.com>
2023-02-17 15:48:01 +0800

    Delete static/js directory

--- static/js/about.c2f9cd41.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self["webpackChunkproject"]=self["webpackChunkproject"]||[]).push([[443],{3242:function(n,t,e){e.r(t),e.d(t,{default:function(){return l}});var r=e(6252);const u={class:"about"},a=(0,r._)("h1",null,"This is an about page",-1),c=[a];function s(n,t){return(0,r.wg)(),(0,r.iD)("div",u,c)}var o=e(3744);const i={},f=(0,o.Z)(i,[["render",s]]);var l=f}}]);
